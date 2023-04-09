a448ae916e01ac9c2d2f8fdc150b8c9c545846cd
Tomekxm <tomekxm@onet.pl>
2023-04-10 00:03:31 +0200

    polish ChooseCardSetWidget

--- lib/clickable/ChooseCardSetWidget.dart
+++ lib/clickable/ChooseCardSetWidget.dart
@@ -6,38 +6,75 @@ class ChooseCardSetWidget extends StatelessWidget {
 
   static String cardSetDirectory = "assets/cardSets/monsters";
   static String? _directory = "";
+  static const double _fontSize = 23;
+  static const double _padding = 15;
 
   @override
   Widget build(BuildContext context) {
     return SimpleDialog(
-      title: const Text('Wybierz zestaw kart!'),
+      title: const Center(
+          child: Text(
+        'Wybierz zestaw kart!',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w900,
+          fontStyle: FontStyle.normal,
+          fontFamily: 'BungeeSpice',
+          fontSize: 33,
+        ),
+      )),
       children: <Widget>[
         SimpleDialogOption(
-          onPressed: () {
-            cardSetDirectory = "assets/cardSets/monsters";
-            Navigator.pop(context);
-          },
-          padding: const EdgeInsets.only(bottom: 10),
-          child: const Text('Potwory'),
-        ),
+            onPressed: () {
+              cardSetDirectory = "assets/cardSets/monsters";
+              Navigator.pop(context);
+            },
+            padding: const EdgeInsets.only(bottom: _padding,top: _padding),
+            child: const Center(
+              child: Text(
+                'Potwory',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: _fontSize,
+                ),
+              ),
+            )),
         SimpleDialogOption(
-          onPressed: () {
-            cardSetDirectory = "assets/cardSets/footballers";
-            Navigator.pop(context);
-          },
-          padding: const EdgeInsets.only(bottom: 10),
-          child: const Text('Piłkarze'),
-        ),
+            onPressed: () {
+              cardSetDirectory = "assets/cardSets/footballers";
+              Navigator.pop(context);
+            },
+            padding: const EdgeInsets.only(bottom: _padding),
+            child: const Center(
+              child: Text(
+                'Piłkarze',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: _fontSize,
+                ),
+              ),
+            )),
         SimpleDialogOption(
-          onPressed: () async {
-            _directory = await FilePicker.platform.getDirectoryPath();
-            if (_directory != null){
-              cardSetDirectory = _directory!;
-            }
-            Navigator.pop(context);
-          },
-          child: const Text('Zaimportuj swój własny zestaw!'),
-        )
+            onPressed: () async {
+              _directory = await FilePicker.platform.getDirectoryPath();
+              if (_directory != null) {
+                cardSetDirectory = _directory!;
+              }
+              Navigator.pop(context);
+            },
+            child: const Center(
+              child: Text(
+                'Zaimportuj swój własny zestaw!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: _fontSize,
+                ),
+              ),
+            ))
       ],
     );
   }
