080fa1146eb7852d42b55944c5b64a259c213ec4
Tomekxm <tomekxm@onet.pl>
2023-05-13 00:39:03 +0200

    add main feature and fancy dialogs

--- lib/clickable/GameCard.dart
+++ lib/clickable/GameCard.dart
@@ -2,11 +2,38 @@ import 'package:flip_card/flip_card.dart';
 import 'package:flip_card/flip_card_controller.dart';
 import 'package:flutter/material.dart';
 
-class GameCard extends StatelessWidget {
-  GameCard({super.key, required this.assetPath});
+class GameCard extends StatefulWidget {
+  GameCard(
+      {super.key, required this.assetPath, required this.onFrontCardFlipDone});
 
-  String assetPath;
-  final FlipCardController flipCardController = FlipCardController();
+  String assetPath = "";
+  void Function(BuildContext context) onFrontCardFlipDone =
+      (BuildContext context) {};
+  FlipCardController flipCardController = FlipCardController();
+
+  @override
+  GameCardState createState() => GameCardState();
+}
+
+class GameCardState extends State<GameCard> {
+  String _assetPath = "";
+  bool _flipOnTouch = true;
+  FlipCardController _flipCardController = FlipCardController();
+  late void Function(BuildContext context) _onFrontCardFlipDone;
+
+  @override
+  void initState() {
+    super.initState();
+    _assetPath = widget.assetPath;
+    _onFrontCardFlipDone = widget.onFrontCardFlipDone;
+    _flipCardController = widget.flipCardController;
+  }
+
+  void changeFlipping() {
+    setState(() {
+      _flipOnTouch = !_flipOnTouch;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +43,18 @@ class GameCard extends StatelessWidget {
           const EdgeInsets.only(left: 1.0, right: 1.0, top: 1.0, bottom: 0.0),
       color: const Color(0x00000000),
       child: FlipCard(
+        flipOnTouch: _flipOnTouch,
         fill: Fill.fillFront,
-        controller: flipCardController,
+        controller: _flipCardController,
         direction: FlipDirection.HORIZONTAL,
         side: CardSide.FRONT,
         speed: 1000,
-        onFlipDone: (status) {},
+        onFlipDone: (status) {
+          changeFlipping();
+          if (status == true) {
+            _onFrontCardFlipDone(context);
+          }
+        },
         front: Container(
           decoration: const BoxDecoration(
             color: Color(0xFF006666),
@@ -41,9 +74,7 @@ class GameCard extends StatelessWidget {
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(assetPath, fit: BoxFit.cover)
-            ],
+            children: <Widget>[Image.asset(_assetPath, fit: BoxFit.cover)],
           ),
         ),
       ),
--- lib/pages/GamePage.dart
+++ lib/pages/GamePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:memoouury/Pages/HomePage.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+import 'package:quickalert/widgets/quickalert_dialog.dart';
 import '../clickable/GameCard.dart';
 import '../helpers/SimpleAssetPathProvider.dart';
 
@@ -12,6 +14,8 @@ class GamePage extends StatelessWidget {
   }
 
   late SimpleAssetPathProvider _assetPathProvider;
+  int _clicks = 0;
+  bool _absorbing = false;
   static const double _fontSize = 20;
   late final int _boardSize;
   late final String _cardSetDirectory;
@@ -19,49 +23,52 @@ class GamePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(),
-            child: IntrinsicHeight(
-                child: Column(children: _buildGameBoard(context))),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FloatingActionButton.extended(
-                heroTag: "return",
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
-                  );
-                },
-                label:
-                    const Text('Powrót', style: TextStyle(fontSize: _fontSize)),
-                icon: const Icon(Icons.keyboard_return),
+    return AbsorbPointer(
+        absorbing: _absorbing,
+        child: Scaffold(
+            body: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(),
+                child: IntrinsicHeight(
+                    child: Column(children: _buildGameBoard(context))),
               ),
-              const Text("    "),
-              FloatingActionButton.extended(
-                heroTag: "restart",
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => GamePage(
-                                boardSize: _boardSize,
-                                cardSetDirectory: _cardSetDirectory,
-                              )));
-                },
-                label: const Text('Restart gry',
-                    style: TextStyle(fontSize: _fontSize)),
-                icon: const Icon(Icons.refresh),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton.extended(
+                    heroTag: "return",
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomePage()),
+                      );
+                    },
+                    label: const Text('Powrót',
+                        style: TextStyle(fontSize: _fontSize)),
+                    icon: const Icon(Icons.keyboard_return),
+                  ),
+                  const Text("    "),
+                  FloatingActionButton.extended(
+                    heroTag: "restart",
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => GamePage(
+                                    boardSize: _boardSize,
+                                    cardSetDirectory: _cardSetDirectory,
+                                  )));
+                    },
+                    label: const Text('Restart gry',
+                        style: TextStyle(fontSize: _fontSize)),
+                    icon: const Icon(Icons.refresh),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ));
+            )));
   }
 
   List<Widget> _buildGameBoard(BuildContext context) {
@@ -79,7 +86,9 @@ class GamePage extends StatelessWidget {
   }
 
   Widget _buildGameCard(BuildContext context) {
-    GameCard card = GameCard(assetPath: _assetPathProvider.provideAssetPath());
+    GameCard card = GameCard(
+        assetPath: _assetPathProvider.provideAssetPath(),
+        onFrontCardFlipDone: _onFrontCardFlipDone);
     _gameCards.add(card);
     return Expanded(
       child: Container(
@@ -90,4 +99,59 @@ class GamePage extends StatelessWidget {
       ),
     );
   }
+
+  Future<void> _onFrontCardFlipDone(BuildContext context) async {
+    _clicks++;
+    if (_clicks % 2 == 0) {
+      List<GameCard> flippedCards = _gameCards
+          .where(
+              (element) => element.flipCardController.controller!.isCompleted)
+          .toList();
+      if (flippedCards.first.assetPath == flippedCards.last.assetPath) {
+        _gameCards.remove(flippedCards.first);
+        _gameCards.remove(flippedCards.last);
+        if (_gameCards.isNotEmpty) {
+          showInfoDialog(context, _gameCards.length ~/ 2);
+        }
+      } else {
+        _absorbing = true;
+        Future.delayed(const Duration(milliseconds: 150), () {
+          flippedCards.first.flipCardController.toggleCard();
+          flippedCards.last.flipCardController.toggleCard();
+        });
+      }
+    }
+    if (_gameCards.isEmpty) {
+      showWinningDialog(context, _clicks);
+    }
+  }
+}
+
+showWinningDialog(BuildContext context, int totalClicks) {
+  QuickAlert.show(
+    title: "Gratulacje!",
+    confirmBtnText: 'Powrót do menu',
+    onConfirmBtnTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    },
+    context: context,
+    type: QuickAlertType.success,
+    text: 'Ukończyłeś grę Memoouury w $totalClicks kliknęciach!',
+  );
+}
+
+showInfoDialog(BuildContext context, int remainingPairs) {
+  QuickAlert.show(
+    title: "Poprawne dopasowanie!",
+    confirmBtnText: 'OK',
+    onConfirmBtnTap: () {
+      Navigator.of(context).pop();
+    },
+    context: context,
+    type: QuickAlertType.info,
+    text: 'Zostało Ci $remainingPairs par kart.',
+  );
 }
